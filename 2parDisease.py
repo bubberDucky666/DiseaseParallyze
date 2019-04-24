d54a3ac6c4b1d3eeb498ae4d9d512f8118e1066e
Jonas <JKTechnical@Jonass-MBP-3.home>
2019-04-24 06:12:09 -0400

    Sending edge particles to and from sNodes finally works

--- 2parDisease.py
+++ 2parDisease.py
@@ -1,6 +1,5 @@
 
-# After move, nodes check other node's particle positions within a bound
-# of 'radius' length. PARTICLE IN NODE CANNOT INFECT PARTICLE OUT OF NODE
+# Try test where only one node can recieve edge lists
 
 import random
 import math as math
@@ -72,6 +71,8 @@ if rank == 0:
 
 	for frame in range(duration):
 		comm.Barrier()
+		print("cat balls")
+		#comm.Barrier()
 
 	print("Done")
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -87,9 +88,6 @@ else:
 	count = 0
 
 	for frame in range(duration):
-		
-		print("\n\n\n\n {}) POOPBUTT \n\n\n".format(rank))
-
 
 		#print("pulling recieved data from other nodes")
 		#for node in range(numNodes**2):
@@ -115,32 +113,87 @@ else:
 
 
 		#=======================================================================
-		#			Movement and Status Changes (w/ Edge Particles)
+		#		   Edge Particle Movement
 
 		move(particleList, mag, nSide)	
 
 		sNodes	 = sNodeGet(pos, numNodes, nodeMatrix)
-		print("{}) sNodes: {}".format(rank, sNodes))
+		if rank == 3: print(sNodes)
+		# print("{}) sNodes: {}".format(rank, sNodes))
 	
-		# Creates a list of edge particles for various nodes
-		# [[n0], [n1], [n2], [n3], [n4], [5n], [n6], [n7], ...]
+		# # Creates a list of edge particles for various nodes
+		# # [[n0], [n1], [n2], [n3], [n4], [5n], [n6], [n7], ...]
 		ePartList = ePartGet(particleList, Node.xBound, Node.yBound, radius)
 
-		# Empty list to be filled with outer-node edge particles
-		oPartList = [[] for i in range(8)]
+		# # Empty list to be filled with outer-node edge particles
+		oPartList = []
 
-		#Sends out edge particles to other Nodes
-		for n in range(len(sNodes)):
-			comm.send(ePartList[n], dest=sNodes[n])
-		print("{}) Done sending edge bois to edge nodes".format(rank))
+		# #Sends out edge particles to other Nodes
+		for n in range(8):
+			#if sNodes[n] == 3:
+				#print("{})sent stuff to 3".format(rank))
+			comm.send(ePartList[n], dest=sNodes[n], tag=0)
+			#print("{}) sent to {}".format(rank, sNodes[n]))
+		
+		print("{}) Sent edge bois to edge nodes".format(rank))
 			
 		#Recieves edge partlices from other nodes 
-		for n in sNodes:
-			nParts = comm.recv(source=n)
-			oPartList.append(nParts)
+		for n in range(8):
+			if rank ==3:
+				nParts = comm.recv(source=sNodes[n], tag=0)
+				oPartList.append(nParts)
+		
+		print("{}) Recieved edge bois from edge nodes".format(rank))		
+		comm.Barrier()
+		print("penisBallFart")
 
-		#comm.Barrier()
+		#========================================================================
+		#				Moving Main Particles Between Nodes
+		
+		#different sublists for different node destinations
+		#if no particles need to be added to a sublist, add None
+		extracts = [[] for i in range(numNodes**2)]      
+		
+		
+		#Checks if each particle needs transfer
+		for indx, p in enumerate(particleList):
+			loc = p.whereAmI(nodeMatrix, nSide, numNodes) #the tag of the node destination
+			newId	 = nodeMatrix[loc[0]][loc[1]].tag
 
+			if newId != rank: #if the node p SHOULD be in is different from its current node
+				
+				#puts particle in proper sublist for desination (slist indice is node rank - 1)
+				extracts[newId-1].append(p)
+				#gets rid of particle from node particle list
+				particleList.pop(indx)
+
+
+		print("{}) sending spEx to other nodes".format(rank))
+		
+		for r in range(numNodes**2):
+			dest = r+1
+
+			#sends out extracts to proper nodes with own rank as a tag
+			comm.send(extracts[r], dest=dest, tag=1)
+			print("{}) sent to {}".format(rank, dest))	
+		
+		#Recieves particle data from each node
+		if rank == 2:
+			for r in range(numNodes**2):
+
+				extracts = comm.recv(source=(r+1), tag=1)
+				print("\n\n{}) Recieved from {}\n\n".format(rank, r+1))
+				#Takes particle from recieved extracts list and adds it to particleList
+				for i in range(len(extracts)):
+					p = extracts[i]
+					particleList.append(p)
+
+		comm.Barrier()
+		#==============================================================================
+
+		#==============================================================================
+		# 					Status and Disease Changes 
+	
 		#Does particle disease checks on local and outernode particles
 		for i in range(len(particleList)):	
 			
@@ -182,55 +235,8 @@ else:
 			for i in range(len(particleList)):
 				part = particleList[i]
 				part.sicken()
-		
-		#========================================================================
-		#				Moving Main Particles Between Nodes
-		
-		#different sublists for different node destinations
-		#if no particles need to be added to a sublist, add None
-		extracts = [[] for i in range(numNodes**2)]      
-		
-		
-		#Checks if each particle needs transfer
-		for indx, p in enumerate(particleList):
-			loc = p.whereAmI(nodeMatrix, nSide, numNodes) #the tag of the node destination
-			newId	 = nodeMatrix[loc[0]][loc[1]].tag
+		#==============================================================================
 
-			if newId != rank: #if the node p SHOULD be in is different from its current node
-				
-				#puts particle in proper sublist for desination (slist indice is node rank - 1)
-				extracts[newId-1].append(p)
-
-				#gets rid of particle from node particle list
-				particleList.pop(indx)
-
-
-		print("{}) sending spEx to other nodes".format(rank))
-		
-		for i in range(len(extracts)):
-			dest = i+1
-			
-			#sends out extracts to proper nodes with own rank as a tag
-			print("{}) sending to {}".format(rank, dest))	
-			comm.send(extracts[i], dest=dest, tag=rank) 
-		
-		#Wait for other nodes to finish
-		print("{})****Waiting****\n".format(rank))
-
-		print("\n\n\n\n {}) PeePeeBUTT \n\n\n".format(rank))
-
-		#Recieves particle data from each node
-		for r in range(numNodes**2):
-			extracts = comm.recv(source=(r+1), tag=(r+1))
-			print("\n\n\n\n {}) PooPooButt \n\n\n".format(rank))
-
-			#Takes particle from recieved extracts list and adds it to particleList
-			for i in range(len(extracts)):
-				p = extracts[i]
-				particleList.append(p)
-
-		comm.Barrier()
-		
 		#Sets particle state lists for graphing 
 		particleList = Node.sPList
 		
@@ -245,38 +251,38 @@ else:
 		
 		print("{}) All extracts properly recieved and sorted".format(rank))
 
-		# xH, yH = coordExstract(healP)
-		# x1, y1 = coordExstract(sick1P)
-		# x2, y2 = coordExstract(sick2P)
-		# x3, y3 = coordExstract(sick3P)
-		# x4, y4 = coordExstract(sick4P)
-		# xD, yD = coordExstract(deadP)
+		xH, yH = coordExstract(healP)
+		x1, y1 = coordExstract(sick1P)
+		x2, y2 = coordExstract(sick2P)
+		x3, y3 = coordExstract(sick3P)
+		x4, y4 = coordExstract(sick4P)
+		xD, yD = coordExstract(deadP)
 
-		# #--------------------------------------------------------------
-		# xB = nodeMatrix[pos[0]][pos[1]].xBound
-		# yB = nodeMatrix[pos[0]][pos[1]].yBound
+		#--------------------------------------------------------------
+		xB = nodeMatrix[pos[0]][pos[1]].xBound
+		yB = nodeMatrix[pos[0]][pos[1]].yBound
 		
-		# #if rank == 3 or rank == 4: # just for debugging
-		# plt.title("Node {}; xB {} yB {}".format(rank, xB, yB))
+		#if rank == 3 or rank == 4: # just for debugging
+		plt.title("Node {}; xB {} yB {}".format(rank, xB, yB))
 
-		# plt.xlim(0, nSide)
-		# plt.ylim(0, nSide)
+		plt.xlim(0, nSide)
+		plt.ylim(0, nSide)
 		
-		# plt.scatter(x1, y1, c='#ADE500')
-		# plt.scatter(xH, yH, c='#00E500')
-		# plt.scatter(x2, y2, c='#E5DF00')
-		# plt.scatter(x3, y3, c='#E5B700')
-		# plt.scatter(x4, y4, c='#E55D00')
-		# plt.scatter(xD, yD, c='#E50018')
+		plt.scatter(x1, y1, c='#ADE500')
+		plt.scatter(xH, yH, c='#00E500')
+		plt.scatter(x2, y2, c='#E5DF00')
+		plt.scatter(x3, y3, c='#E5B700')
+		plt.scatter(x4, y4, c='#E55D00')
+		plt.scatter(xD, yD, c='#E50018')
 
-		# #Draw outline of domain
-		# plt.plot([ xB[0], xB[0] ],[ yB[1], yB[0] ])
-		# plt.plot([ xB[1], xB[1] ],[ yB[1], yB[0] ])
-		# plt.plot([ xB[0], xB[1] ],[ yB[0], yB[0] ])
-		# plt.plot([ xB[0], xB[1] ],[ yB[1], yB[1] ])
+		#Draw outline of domain
+		plt.plot([ xB[0], xB[0] ],[ yB[1], yB[0] ])
+		plt.plot([ xB[1], xB[1] ],[ yB[1], yB[0] ])
+		plt.plot([ xB[0], xB[1] ],[ yB[0], yB[0] ])
+		plt.plot([ xB[0], xB[1] ],[ yB[1], yB[1] ])
 		
-		# plt.draw()
-		# plt.pause(pause)
-		# plt.clf()
+		plt.draw()
+		plt.pause(pause)
+		plt.clf()
 
 
